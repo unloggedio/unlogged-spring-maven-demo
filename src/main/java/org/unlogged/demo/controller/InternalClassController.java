4b39f0ef2bb150ef371b31e86a70cb9306bd635f
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2024-02-09 19:08:33 +0530

    Adding cases to test around timestamp objects, 613

--- src/main/java/org/unlogged/demo/controller/InternalClassController.java
+++ src/main/java/org/unlogged/demo/controller/InternalClassController.java
@@ -1,6 +1,14 @@
 package org.unlogged.demo.controller;
 
+import com.sun.jna.platform.win32.Sspi;
 import lombok.Getter;
+import org.joda.time.DateTime;
+import org.joda.time.Instant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InternalClassController {
 
@@ -16,5 +24,31 @@ public class InternalClassController {
         public int methodE2() {
             return subInstance.getY();
         }
+    }
+
+    private List<String> getTimeObjects() {
+        List<String> timestamps = new ArrayList<>();
+        DateTime jodaDateTime = new DateTime();
+        timestamps.add(jodaDateTime.toString());
+        Sspi.TimeStamp sspiTimeStamp = new Sspi.TimeStamp();
+        timestamps.add(sspiTimeStamp.toString());
+        Instant instant = new Instant();
+        timestamps.add(instant.toString());
+        java.time.Instant timeInstant = java.time.Instant.now();
+        timestamps.add(timeInstant.toString());
+        return timestamps;
+    }
+
+    private final Map<String, String> map1 = new HashMap<>() {
+        {
+            put("Val1", "key1");
+            put("Val2", "key2");
+            put("Val3", "key3");
+            put("Val4", "key4");
+        }
+    };
+
+    private String getMapValue() {
+        return map1.get("Val4");
     }
 }
