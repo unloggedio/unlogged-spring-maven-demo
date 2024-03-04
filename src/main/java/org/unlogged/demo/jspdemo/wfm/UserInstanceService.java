38266ff1e48bc96abed8aff2a79382d1318998c2
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2024-03-04 19:40:14 +0530

    Removing mock bindings for non mocks that don't exist

--- src/main/java/org/unlogged/demo/jspdemo/wfm/UserInstanceService.java
+++ src/main/java/org/unlogged/demo/jspdemo/wfm/UserInstanceService.java
@@ -5,17 +5,15 @@ import java.util.Optional;
 public class UserInstanceService {
 
     UserInstanceDao dao = new UserInstanceDao();
-    public UserInstance saveUser(String username, String password, String email)
-    {
-        UserInstance instance = new UserInstance(username,password,email);
+
+    public UserInstance saveUser(String username, String password, String email) {
+        UserInstance instance = new UserInstance(username, password, email);
         return dao.save(instance);
     }
 
-    public UserInstance fetchUser(int userid)
-    {
-        Optional<UserInstance> instance =  dao.get(userid);
-        if(instance.isPresent())
-        {
+    public UserInstance fetchUser(int userid) {
+        Optional<UserInstance> instance = dao.get(userid);
+        if (instance.isPresent()) {
             return instance.get();
         }
         return null;
