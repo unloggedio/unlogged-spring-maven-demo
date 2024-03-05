67981ad12b43c84a42bcc2bb2c1a06c404a63b9f
Kartikey Tewari <kt@unlogged.io>
2024-03-05 12:28:03 +0530

    remove unlogged test runner

--- src/test/java/UnloggedRunnerTest.java
+++ src/test/java/UnloggedRunnerTest.java
@@ -1,31 +1,31 @@
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.core.io.support.PropertySourceFactory;
-import org.springframework.test.context.TestPropertySource;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.core.env.PropertiesPropertySource;
+// import org.springframework.core.io.support.EncodedResource;
+// import org.springframework.core.io.support.PropertySourceFactory;
+// import org.springframework.test.context.TestPropertySource;
 
-import io.unlogged.runner.UnloggedTestRunner;
+// import io.unlogged.runner.UnloggedTestRunner;
 
-@RunWith(UnloggedTestRunner.class)
-@ComponentScan("org.unlogged.demo")
-@EnableAutoConfiguration
-@TestPropertySource({"classpath:application.properties"})
-public class UnloggedRunnerTest {
+// @RunWith(UnloggedTestRunner.class)
+// @ComponentScan("org.unlogged.demo")
+// @EnableAutoConfiguration
+// @TestPropertySource({"classpath:application.properties"})
+// public class UnloggedRunnerTest {
 
-    public static class YamlPropertySourceFactory implements PropertySourceFactory {
-        public YamlPropertySourceFactory() {
-        }
+//     public static class YamlPropertySourceFactory implements PropertySourceFactory {
+//         public YamlPropertySourceFactory() {
+//         }
 
-        @Override
-        public PropertiesPropertySource createPropertySource(String name, EncodedResource encodedResource) throws IOException {
-            YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-            factory.setResources(encodedResource.getResource());
-            return new PropertiesPropertySource(encodedResource.getResource().getFilename(), factory.getObject());
-        }
-    }
-}
+//         @Override
+//         public PropertiesPropertySource createPropertySource(String name, EncodedResource encodedResource) throws IOException {
+//             YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+//             factory.setResources(encodedResource.getResource());
+//             return new PropertiesPropertySource(encodedResource.getResource().getFilename(), factory.getObject());
+//         }
+//     }
+// }
