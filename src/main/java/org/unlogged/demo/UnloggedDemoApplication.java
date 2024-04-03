196c0f447f53183b72d72ffc899846f038b2c82f
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2024-04-03 16:03:03 +0530

    prepping project for ui tests

--- src/main/java/org/unlogged/demo/UnloggedDemoApplication.java
+++ src/main/java/org/unlogged/demo/UnloggedDemoApplication.java
@@ -1,6 +1,5 @@
 package org.unlogged.demo;
 
-import io.unlogged.Unlogged;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -8,9 +7,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class UnloggedDemoApplication {
-    @Unlogged
     public static void main(String[] args) {
         SpringApplication.run(UnloggedDemoApplication.class, args);
     }
-
 }
