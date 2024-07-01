a9f38c67f72c1d3398b5f898902ca8070b4c5ab9
Kartikey Tewari <kt@unlogged.io>
2024-07-01 18:14:58 +0530

    add otel sdk config

---
+++ src/main/java/org/unlogged/demo/OpenTelemetryConfigSDK.java
@@ -0,0 +1,57 @@
+package org.unlogged.demo;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@Profile("sdk")
+public class OpenTelemetryConfigSDK {
+
+    @Value("${otel.exporter.jaeger.endpoint}")
+    private String jaegerEndpoint;
+
+    @Value("${otel.service.name}")
+    private String serviceName;
+
+    @Bean
+    public OpenTelemetry initOpenTelemetry() {
+
+        // Define exporter Jaeger config
+        SpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
+                .setEndpoint(jaegerEndpoint)
+                .build();
+
+        // Define tracerProvider
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter)
+                        .setScheduleDelay(100, TimeUnit.MILLISECONDS)
+                        .build())
+                .build();
+
+        // Define openTelemetry
+        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+                .setTracerProvider(tracerProvider)
+                .buildAndRegisterGlobal();
+
+        return openTelemetry;
+    }
+
+
+    @Bean
+    public Tracer tracer (OpenTelemetry openTelemetry) {
+
+        // Define tracer
+        return openTelemetry.getTracer(serviceName);
+    }
+}
