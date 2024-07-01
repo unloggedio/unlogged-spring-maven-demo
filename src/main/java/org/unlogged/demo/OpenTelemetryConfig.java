15c18d817c4c0965344d36bb90000eea19f760a4
Kartikey Tewari <kt@unlogged.io>
2024-07-01 17:55:52 +0530

    update opentelemetry SDK config

--- src/main/java/org/unlogged/demo/OpenTelemetryConfig.java
+++ src/main/java/org/unlogged/demo/OpenTelemetryConfig.java
@@ -1,6 +1,5 @@
 package org.unlogged.demo;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
@@ -8,42 +7,55 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.concurrent.TimeUnit;
 
-@Component
+@Configuration
+@Profile("sdk")
 public class OpenTelemetryConfig {
 
-    private static Tracer tracer;
+    @Bean
+    public OpenTelemetry initOpenTelemetry() {
 
-    @PostConstruct
-    public void init() {
-
-        // Jaeger config
+        // Define exporter Jaeger config
         SpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
-                .setEndpoint("http://localhost:14250")
+                .setEndpoint("http://localhost:4317")
                 .build();
 
+        // Define tracerProvider
         SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
                 .addSpanProcessor(BatchSpanProcessor.builder(spanExporter)
                         .setScheduleDelay(100, TimeUnit.MILLISECONDS)
                         .build())
                 .build();
 
-        // Build and register OpenTelemetry instance
-        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+        // Define openTelemetry
+        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
                 .setTracerProvider(tracerProvider)
                 .buildAndRegisterGlobal();
 
-        // use traces for implementation
-        OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
-        tracer = openTelemetry.getTracer("io.opentelemetry.example");
+        return openTelemetry;
     }
 
-    public static Tracer getTracer() {
-        return tracer;
-    }
 
+    @Bean
+    public Tracer tracer (OpenTelemetry openTelemetry) {
+
+        // Define tracer
+        return openTelemetry.getTracer("org.unlogged.demo");
+    }
 }
+
+
+// To load the tracer in PerfController:
+
+
+//    private final Tracer tracer;
+//
+//    @Autowired
+//    public PerfController(Tracer tracer) {
+//        this.tracer = tracer;
+//    }
