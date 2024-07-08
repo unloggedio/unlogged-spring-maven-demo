9faaef162cb8f362c7a4bc87dfb27726dc0bb60e
Kartikey Tewari <kt@unlogged.io>
2024-07-08 15:38:44 +0530

    add method registration in perf controller

--- src/main/java/org/unlogged/demo/controller/PerfController.java
+++ src/main/java/org/unlogged/demo/controller/PerfController.java
@@ -13,20 +13,21 @@ import org.unlogged.demo.service.PerfService;
 import java.util.ArrayList;
 
 import static org.unlogged.demo.OtelConfig.makeSpan;
+import static org.unlogged.demo.OtelConfig.registerMethod;
 
 
 @RestController
 @RequestMapping("/perf")
 public class PerfController {
 
-    private final Tracer tracer = GlobalOpenTelemetry.getTracer("unlogged-spring-maven-demo");
+    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("unlogged-spring-maven-demo");
 
 	@Autowired
     private PerfService perfService;
 
     @RequestMapping("/ping")
     public String ping() {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.1").startSpan();
 
         String s = "server up!";
         makeSpan(span, "output", s);
@@ -37,7 +38,7 @@ public class PerfController {
 
     @RequestMapping("/cpuintensive")
     public long cpu(@RequestParam long value) {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.2").startSpan();
         makeSpan(span, "input.value", value);
 
         long val =  perfService.getCpuIntensiveData(value);
@@ -50,7 +51,7 @@ public class PerfController {
 
     @RequestMapping("/memoryintensive1")
     public long memoryIntensive1(@RequestParam int count) {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.3").startSpan();
         makeSpan(span, "input.count", count);
 
         long val =  perfService.sum_natural(count);
@@ -63,7 +64,7 @@ public class PerfController {
 
     @RequestMapping("/memoryintensive2")
     public String memoryIntensive2(@RequestParam int count) {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.4").startSpan();
         makeSpan(span, "intput.count", count);
 
         String val = perfService.readWriteInMemory(count);
@@ -76,7 +77,7 @@ public class PerfController {
 
     @RequestMapping("/networkintensive")
     public String networkintensive(@RequestParam int count) {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.5").startSpan();
         makeSpan(span, "input.count", count);
 
         String value = perfService.genManyNetworkCall(count);
@@ -89,7 +90,7 @@ public class PerfController {
 
     @RequestMapping("/databaseintensive")
     public String databaseintensive(@RequestParam int count) {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.6").startSpan();
         makeSpan(span, "input.count", count);
 
         String value = perfService.genDatabaseIntensive(count);
@@ -102,7 +103,7 @@ public class PerfController {
 
     @RequestMapping("/dataIntensive")
     public String dataIntensive(int count) {
-        Span span = tracer.spanBuilder("custom_tracer").startSpan();
+        Span span = tracer.spanBuilder("custom_tracer.7").startSpan();
         makeSpan(span, "input.count", count);
         // This method triggers a method with many large pojos as input
         // That method has many external calls. This benchmarks OTEL span data collection performance
@@ -118,5 +119,17 @@ public class PerfController {
         makeSpan(span, "output", s);
         span.end();
         return s;
+    }
+
+    static {
+        Span span = tracer.spanBuilder("method_registration").startSpan();
+        registerMethod(span, 1, "org.unlogged.demo.controller.PerfController", "ping", "", "java.lang.String", false, true, false, 1, "()Ljava/lang/String;");
+        registerMethod(span, 2, "org.unlogged.demo.controller.PerfController", "cpu", "J", "J", false, true, false, 1, "(J)J");
+        registerMethod(span, 3, "org.unlogged.demo.controller.PerfController", "memoryIntensive1", "I", "J", false, true, false, 1, "(I)J");
+        registerMethod(span, 4, "org.unlogged.demo.controller.PerfController", "memoryIntensive2", "I", "java.lang.String", false, true, false, 1, "(I)Ljava/lang/String;");
+        registerMethod(span, 5, "org.unlogged.demo.controller.PerfController", "networkintensive", "I", "java.lang.String", false, true, false, 1, "(I)Ljava/lang/String;");
+        registerMethod(span, 6, "org.unlogged.demo.controller.PerfController", "databaseintensive", "I", "java.lang.String", false, true, false, 1, "(I)Ljava/lang/String;");
+        registerMethod(span, 7, "org.unlogged.demo.controller.PerfController", "dataIntensive", "I", "java.lang.String", false, true, true, 1, "(I)Ljava/lang/String;");
+        span.end();
     }
 }
