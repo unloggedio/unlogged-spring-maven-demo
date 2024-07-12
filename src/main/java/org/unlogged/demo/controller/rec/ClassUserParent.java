6880465a5eb8f84cef39acf54bcc99aac3024fe0
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2024-07-12 17:46:29 +0530

    missed commit

---
+++ src/main/java/org/unlogged/demo/controller/rec/ClassUserParent.java
@@ -0,0 +1,16 @@
+package org.unlogged.demo.controller.rec;
+
+import org.unlogged.demo.jspdemo.wfm.Models.Entities.User;
+
+public class ClassUserParent {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
