e9f819cd33f07c501a4b2c965b115827a3a61577
Kartikey Tewari <kt@unlogged.io>
2024-08-09 16:46:35 +0530

    fix name

--- src/main/java/org/unlogged/demo/jspdemo/wfm/Dao/UsersRepository.java
+++ src/main/java/org/unlogged/demo/jspdemo/wfm/Dao/UsersRepository.java
@@ -6,9 +6,9 @@ import org.unlogged.demo.jspdemo.wfm.Models.Entities.User;
 
 public interface UsersRepository extends JpaRepository<User,Long> {
 
-    @Query(value = "SELECT u.user_id as user_id, u.username as username, u.password as password, u.email as email FROM Users u WHERE username = :username", nativeQuery = true)
+    @Query(value = "SELECT u.user_id as user_id, u.username as username, u.password as password, u.email as email FROM users u WHERE username = :username", nativeQuery = true)
     public User getUserByUsername(String username);
 
-    @Query(value = "SELECT u.user_id as user_id, u.username as username, u.password as password, u.email as email FROM Users u WHERE user_id = :id", nativeQuery = true)
+    @Query(value = "SELECT u.user_id as user_id, u.username as username, u.password as password, u.email as email FROM users u WHERE user_id = :id", nativeQuery = true)
     public User getUserByUserId(long id);
 }
